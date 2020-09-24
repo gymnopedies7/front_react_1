963c4b87f0458a7174c56648b42d64f8c05500ce
Hwang <gymnopedies7@naver.com>
2020-09-25 02:40:43 +0900

    first commit django with react

---
+++ blog1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    qs = Post.objects.all()
+    return render(request, 'blog1/post_list.html', {
+        'post_list' : qs,
+    })
