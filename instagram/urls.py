d043812e62bb10523a7e3b7b59ccde40b1f6d7ec
gymnopedies7 <gymnopedies7@naver.com>
2021-01-24 19:59:41 +0900

    changed

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.post_list),
+    path('<int:pk>/', views.post_detail),       # pk가 int로 넘어옴
+    #re_path(r'(?P<pk>\d+)$', views.post_detail),   < 위와 동일함 (pk가 문자열로 넘어옴)
+    #path('archives/<int:year>/', views.archives_year),
+    re_path(r'archives/(?P<year>\d{4})/', views.archives_year),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,8 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Post
+
+#post_list=  ListView.as_view(model=Post)  : 아래와 동일하게 할수있으나, 너무 마법같고, 추후 커스터마이징이 안됨
 
 def post_list(request):
     qs = Post.objects.all()
@@ -9,3 +12,9 @@ def post_list(request):
     # instagram/templates/instagram/post_list.html
     return render(request, 'instagram/post_list.html',{'post_list':qs, 'q':q, } )
 
+def archives_year(request, year):
+    return HttpResponse(f"{year}년 archives")
+
+
+def post_detail(request,pk):
+    pass
